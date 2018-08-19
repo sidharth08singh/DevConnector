39bc5cf46aa4bdd1139fa3b73986a9a82fb34dbd
Sidharth Singh <sidharth.singh@logmein.com>
2018-08-18 19:54:17 -0700

    Add routes for get and delete posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,7 +4,9 @@ const passport = require("passport");
 const mongoose = require("mongoose");
 
 //Load Post Model
-const Profile = require("../../models/Post");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+
 
 //Load Post Validation
 const validatePostInput = require("../../validation/post");
@@ -18,15 +20,59 @@ const validatePostInput = require("../../validation/post");
 router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
 
 /**
+ * @route   GET api/posts
+ * @desc    GET posts
+ * @access  Public
+ */
+ router.get("/", (req, res) => {
+   Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json( { nopostsfound: "No posts found with that ID" }));
+});
+
+/**
+ * @route   GET api/posts/:id
+ * @desc    GET post by id
+ * @access  Public
+ */
+ router.get("/:id", (req, res) => {
+   Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404).json( { nopostfound: "No post found with that ID" }));
+});
+
+/**
+ * @route   DELETE api/posts/:id
+ * @desc    DELETE post by id
+ * @access  Public
+ */
+ router.delete("/:id", passport.authenticate("jwt", { session: false }), (req, res) => {
+   Profile.findOne( { user: req.user.id })
+    .then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ notauthorized: "User not authorized"});
+          }
+          // Delete
+          post.remove().then(() => res.json( { success: true}));
+        })
+        .catch(err => res.status(404).json( { postnotfound: "No post found" }))
+      });
+  });
+
+
+
+/**
  * @route   POST api/posts
  * @desc    Create posts
  * @access  Private
  */
  router.post("/", passport.authenticate("jwt", { session: false }),(req, res) => {
     const { errors, isValid } = validatePostInput(req.body);
-
-    console.log(errors);
-
+    
     // Check validation
     if (!isValid) {
       // If any errors, send 400 with error object
